e67220517310aad2737461e7ff3ac1f51042d367
Fernando Ruan <fernandosingaporefuture@gmail.com>
2025-10-16 00:27:10 -0300

    ok

---
+++ FULLSTACK/labs/mvc/controllers/usersControllers.js
@@ -0,0 +1,22 @@
+const userModels = require("../models/userModels.js");
+
+exports.getHomePage = (req, res) => {
+	const users = userModels.getAllUsers();
+	res.render("users", { users });
+}
+
+exports.addUser = (req, res) => {
+	const { name, age } = req.body;
+	
+	// Protect because it cannot be safe
+
+	if (!name || !age ) {
+		return res.status(400).send("Name and age are required!");
+	}
+
+	// Call the database to add the data
+
+	userModel.addUser(name, age);
+
+	res.render("success", { name, age });
+}
